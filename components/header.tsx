ed507200e137e619796dc14425ab2e32af08b6fd
Armando Ruiz <armando_ruiizz@hotmail.com>
2025-04-24 18:56:01 -0600

    fix component name

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import useActiveSectionContext from '@/hooks/useActiveSectionContext';
 
-export default function header() {
+export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
 
